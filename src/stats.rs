35d2e93041ade7167770785e917ae009333557f7
Sjael <jakeobrien44@gmail.com>
2023-06-30 21:34:05 -0700

    undid sort change, i am a fool

--- src/stats.rs
+++ src/stats.rs
@@ -189,7 +189,7 @@ pub fn calculate_attributes(
     for (entity, mut attributes) in &mut attributes {
         // sort by deepest modifier, so we process Mul<Add<Mul<Base<Health>>>> before Mul<Base<Health>>
         let mut tags = attributes.keys().cloned().collect::<Vec<_>>();
-        tags.sort_by(|a, b| b.deepness().cmp(&a.deepness()));
+        tags.sort_by(|a, b| a.deepness().cmp(&b.deepness()));
 
         for tag in tags {
             match tag.clone() {
